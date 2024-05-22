92902f43811fa86359cb0128b2597593a1ba59f3
Carole Desiree Cachero <cdcachero@myseneca.ca>
2024-05-22 11:44:36 -0400

    added labs2a-g

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Carole Desiree Cachero
+#Author ID: cdcachero
+#Date Created: 2024-05-22
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
---
+++ lab2g.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Author: Carole Desiree Cachero
+#Author ID: cdcachero
+#Date Created: 2024-05-22
+
+
+import sys
+
+
+if len(sys.argv) == 1:
+    timer = 3    
+
+else:
+    timer = int(sys.argv[1])
+
+while timer !=0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
